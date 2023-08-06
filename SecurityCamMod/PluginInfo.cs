88b61e8b088598271a7aac69f44f2eb0cc42fb09
ogga hogga <103965225+oggahogga@users.noreply.github.com>
2023-08-06 04:17:12 -0400

    Add files via upload

---
+++ SecurityCamMod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SecurityCamMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.oggahogga.gorillatag.securitycammod";
+        public const string Name = "SecurityCamMod";
+        public const string Version = "1.0.0";
+    }
+}
